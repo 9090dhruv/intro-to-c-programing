4ae4eec1c265d127e1dbe0647b66e61f2de2a078
9090dhruv <dhruvpraj9999@gmail.com>
2025-08-29 09:34:07 +0530

    Add files via upload

---
+++ dhruv6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0, i = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    while(count < n) {
+        if(i % 2 != 0) {
+            printf("%d\n", i);
+            count++;
+        }
+        i++;
+    }
+
+    return 0;
+}
